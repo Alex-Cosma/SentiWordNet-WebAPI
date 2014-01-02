eff2bd2fa0136e5e0c4f2646883f74f7af7e4b20
alexcosma <alexandru.cosma@catalysts.cc>
2014-01-02 02:07:34 +0200

    Added gitignore for intellij

---
+++ src/groovy/com/sentiwordnet/web/api/helpers/Paths.groovy
@@ -0,0 +1,14 @@
+package com.sentiwordnet.web.api.helpers
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexcosma
+ * Date: 01.01.14
+ * Time: 15:33
+ * To change this template use File | Settings | File Templates.
+ */
+class Paths {
+
+    public static String SENTIWORDNET_FILEPATH;
+
+}
